f62c10290255997b70833303ad6de7ec79fba969
Kaan Ata İnanç <kaanatainanc@gmail.com>
2025-10-24 18:04:03 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Uçuş_Rezervasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            label9 = new Label();
+            button2 = new Button();
+            label4 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            label7 = new Label();
+            label6 = new Label();
+            maskedTextBox3 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label8 = new Label();
+            listBox2 = new ListBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Location = new Point(12, 120);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(394, 283);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Uçuş Bilgileri";
+            groupBox1.Enter += groupBox1_Enter_1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(338, 14);
+            label9.Name = "label9";
+            label9.Size = new Size(50, 20);
+            label9.TabIndex = 11;
+            label9.Text = "label9";
+            label9.Visible = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Linen;
+            button2.ForeColor = SystemColors.MenuText;
+            button2.Location = new Point(323, 74);
+            button2.Name = "button2";
+            button2.Size = new Size(65, 59);
+            button2.TabIndex = 10;
+            button2.Text = "< > ";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 218);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Uçuş Saati";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(114, 218);
+            maskedTextBox1.Mask = "00:00";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(250, 27);
+            maskedTextBox1.TabIndex = 8;
+            maskedTextBox1.ValidatingType = typeof(DateTime);
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(114, 166);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Yolculuk Tarihi";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Nereye";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Nereden";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Ankara", "Adana", "İstanbul", "Elazığ", "Samsun" });
+            comboBox2.Location = new Point(78, 111);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(225, 28);
+            comboBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Ankara", "Adana", "İstanbul", "Elazığ", "Samsun" });
+            comboBox1.Location = new Point(78, 69);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(225, 28);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(maskedTextBox3);
+            groupBox2.Controls.Add(maskedTextBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Location = new Point(472, 134);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(403, 269);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Yolcu Bilgileri";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkRed;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(149, 198);
+            button1.Name = "button1";
+            button1.Size = new Size(155, 55);
+            button1.TabIndex = 11;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(73, 158);
+            label7.Name = "label7";
+            label7.Size = new Size(61, 20);
+            label7.TabIndex = 10;
+            label7.Text = "Telefon;";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(93, 111);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 20);
+            label6.TabIndex = 9;
+            label6.Text = "TC;";
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(140, 151);
+            maskedTextBox3.Mask = "(999) 000-0000";
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(237, 27);
+            maskedTextBox3.TabIndex = 8;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(140, 103);
+            maskedTextBox2.Mask = "00000000000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(237, 27);
+            maskedTextBox2.TabIndex = 7;
+            maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(140, 54);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(237, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(19, 61);
+            label5.Name = "label5";
+            label5.Size = new Size(115, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Yolcu Ad Soyad;";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Info;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label8);
+            panel1.Location = new Point(13, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(866, 105);
+            panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(594, -3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(269, 108);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(253, 105);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Sitka Text", 13.7999992F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label8.Location = new Point(268, 39);
+            label8.Name = "label8";
+            label8.Size = new Size(282, 33);
+            label8.TabIndex = 5;
+            label8.Text = " JÜPİTER HAVA YOLLARI";
+            label8.Click += label8_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.HorizontalScrollbar = true;
+            listBox2.Location = new Point(12, 415);
+            listBox2.Name = "listBox2";
+            listBox2.ScrollAlwaysVisible = true;
+            listBox2.Size = new Size(871, 224);
+            listBox2.TabIndex = 8;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(891, 653);
+            Controls.Add(listBox2);
+            Controls.Add(panel1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Uçuş Rezervasyon";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label4;
+        private MaskedTextBox maskedTextBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label5;
+        private MaskedTextBox maskedTextBox3;
+        private MaskedTextBox maskedTextBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label7;
+        private Label label6;
+        private ListBox listBox1;
+        private Panel panel1;
+        private Label label8;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private ListBox listBox2;
+        private Button button2;
+        private Label label9;
+    }
+}
